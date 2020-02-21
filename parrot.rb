d681b27c882d512cade5660d9dacf644b532a6b5
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-21 20:34:58 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-  puts "Squawk!"
-  "Squawk!"
+def parrot(noise)
+  puts noise
+  noise
 end
+noise = "Squawk!"
